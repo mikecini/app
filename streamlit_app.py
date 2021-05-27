18ff44102fae0a47bb09dd2768b23caeb99ad7f0
mikecini <56175997+mikecini@users.noreply.github.com>
2021-05-27 16:58:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,9 @@ import ffn
 st.title('Global Equity Performance Tool')
 st.header('This is O14s app')
 
+st.selectbox ('Select 2 Funds',[AGF Global Select, Fidelity Global Innovators,Dynamic Power Global Growth])
+
+
 prices = ffn.get('0P000073QD.TO,0P0000737Y.TO', start='2008-01-01')
 ax = prices.rebase()
 
