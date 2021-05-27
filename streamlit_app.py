eb6efff15a0f95c18fee6007124a9e6b7e2dfecb
mikecini <56175997+mikecini@users.noreply.github.com>
2021-05-27 16:26:24 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import quantstats as qs
 import streamlit as st
 import quantstats as qs
 
+st.image('https://pbs.twimg.com/profile_images/973653394320646144/1ujH6gL6_400x400.jpg')
+
 st.title('O14')
 st.header('This is O14s app')
 
