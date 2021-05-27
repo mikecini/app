8dae61c5cf4eadde565d535ea3e3f6838c0e8b40
mikecini <56175997+mikecini@users.noreply.github.com>
2021-05-27 16:22:01 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import quantstats as qs
 import streamlit as st
+import quantstats as qs
 
 st.title('O14')
 st.header('This is O14s app')
+
+qs.extend_pandas()
+stock = qs.utils.download_returns('0P000073QD.TO')
+
+st.line_chart(stock)
