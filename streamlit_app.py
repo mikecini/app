cd45185679a34943dcfab03fc02440c563d539be
mikecini <56175997+mikecini@users.noreply.github.com>
2021-05-28 15:06:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,10 @@
 import streamlit as st
 import ffn
-import pylab
+
 
 stocks = ["0P000073QD.TO", "0P0000737Y.TO", "0P0001C8AE.TO", "0P0000IUYO.TO"]
 
 check_boxes = [st.sidebar.checkbox(stock, key=stock) for stock in stocks]
-
 checked_stocks = [stock for stock, checked in zip(stocks, check_boxes) if checked]
 
 for stock in checked_stocks:
