645f710c9a2cee1102f72d6333b83eb5c50f9b17
mikecini <56175997+mikecini@users.noreply.github.com>
2021-05-28 11:21:38 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,6 @@ EDG = st.sidebar.checkbox  ('Edgepoint Global Portfolio')
 MAW = st.sidebar.checkbox  ('Mawer Global Equity')
 CAP = st.sidebar.checkbox  ('Capital Group Global Equity Canada')
 
-if st.checkbox
 
 prices = ffn.get('0P000073QD.TO,0P0000737Y.TO', start='2008-01-01')
 ax = prices.rebase()
