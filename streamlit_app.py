51b324acf4ee041ea5da342cd2544346868d890d
mikecini <56175997+mikecini@users.noreply.github.com>
2021-05-28 11:18:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,13 @@ import ffn
 st.title('Global Equity Performance Tool')
 st.header('This is O14s app')
 
-st.checkbox ('AGF Global Select')
-st.checkbox ('FIDO Gloabl Innovators')
-st.checkbox ('Dynamic Power Global Growth')
-st.checkbox ('Edgepoint Global Portfolio')
-st.checkbox ('Mawer Global Equity')
-st.checkbox ('Capital Group Global Equity Canada')
+GS = st.checkbox ('AGF Global Select')
+FID = st.checkbox ('FIDO Gloabl Innovators')
+DYN = st.checkbox ('Dynamic Power Global Growth')
+EDG = st.checkbox ('Edgepoint Global Portfolio')
+MAW = st.checkbox ('Mawer Global Equity')
+CAP = st.checkbox ('Capital Group Global Equity Canada')
+
 
 
 prices = ffn.get('0P000073QD.TO,0P0000737Y.TO', start='2008-01-01')
