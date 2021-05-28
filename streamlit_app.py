fb74942927d3088fc0b61b1c355e84db8b0435b2
mikecini <56175997+mikecini@users.noreply.github.com>
2021-05-28 11:23:53 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,18 +2,14 @@ import streamlit as st
 import ffn
 #%pylab inline
 
-st.title('Global Equity Performance Tool')
-st.header('This is O14s app')
+stocks = ["AGF Global Select", "Dynamic Power Global Growth", "Fidelity Global Innovators", "Edgepoint Global Portfolio"]
 
-GS = st.sidebar.checkbox ('AGF Global Select')
-FID = st.sidebar.checkbox  ('FIDO Gloabl Innovators')
-DYN = st.sidebar.checkbox  ('Dynamic Power Global Growth')
-EDG = st.sidebar.checkbox  ('Edgepoint Global Portfolio')
-MAW = st.sidebar.checkbox  ('Mawer Global Equity')
-CAP = st.sidebar.checkbox  ('Capital Group Global Equity Canada')
+check_boxes = [st.sidebar.checkbox(stock, key=stock) for stock in stocks]
 
+st.write([stock for stock, checked in zip(stocks, check_boxes) if checked])
 
-prices = ffn.get('0P000073QD.TO,0P0000737Y.TO', start='2008-01-01')
-ax = prices.rebase()
+checked_stocks = [stock for stock, checked in zip(stocks, check_boxes) if checked]
 
-st.line_chart(ax)
+if st.button("Download data"):
+    for stock in checked_stocks:
+        download_data(stock)
