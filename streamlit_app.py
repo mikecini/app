22b0a231b7d7e11f19b7816642a6ddc59a84670e
mikecini <56175997+mikecini@users.noreply.github.com>
2021-05-28 15:18:52 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ stocks = ["0P000073QD.TO", "0P0000737Y.TO", "0P0001C8AE.TO", "0P0000IUYO.TO"]
 check_boxes = [st.sidebar.checkbox(stock, key=stock) for stock in stocks]
 checked_stocks = [stock for stock, checked in zip(stocks, check_boxes) if checked]
 
-prices = ffn.get(stock, start='2008-01-01').dropna()
+prices = ffn.get(checked_stocks, start='2008-01-01').dropna()
 
 lookback = ffn.core.calc_perf_stats(prices)
   
