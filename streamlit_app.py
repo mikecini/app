0a38869f0be1bbe746f050395c132ee3ca2c131d
mikecini <56175997+mikecini@users.noreply.github.com>
2021-05-31 12:33:05 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import ffn
 
-GS = st.checkbox('AGF Global Select', value = 'True')
+GS = st.checkbox('AGF Global Select')
 EDG = st.checkbox('Edgepoint Global Portfolio')
 DYN = st.checkbox('Dynamic Power Global Growth Portfolio')
 FID = st.checkbox('Fidelity Global Innovators')
