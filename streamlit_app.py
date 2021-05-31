ad0ca873f9d99b25c745953896051a1f401f8fdc
mikecini <56175997+mikecini@users.noreply.github.com>
2021-05-31 13:26:21 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,7 @@ FID = st.checkbox('Fidelity Global Innovators')
 
 if GS and EDG is True:
   price_data = ffn.get('0P000073QD.TO,0P0000IUYO.TO')
+  price_data.columns = ['AGF Global Select','Edgepoint Global Portfolio']
   log_ret = np.log(price_data/price_data.shift(1))
   cov_mat = log_ret.cov() * 252
   num_port = 5000
@@ -81,7 +82,37 @@ if GS and DYN is True:
   st.table(max_sr)
   
 if GS and FID is True:
-  GS_FID = ffn.get('0P000073QD.TO,0P0001C8AE.TO').to_returns().dropna()
-  GS_FID.columns=['AGF Global Select','Fidelity Global Innovators']
-  weights = ffn.core.calc_mean_var_weights(GS_FID, weight_bounds=(min_weight, max_weight), rf=0.0)
-  st.table (weights)
+  price_data = ffn.get('0P000073QD.TO,0P0001C8AE.TO')
+  price_data.columns = ['AGF Global Select','Fidelity Global Innovators']
+  log_ret = np.log(price_data/price_data.shift(1))
+  cov_mat = log_ret.cov() * 252
+  num_port = 5000
+  all_wts = np.zeros((num_port, len(price_data.columns)))
+  port_returns = np.zeros((num_port))
+  port_risk = np.zeros((num_port))
+  sharpe_ratio = np.zeros((num_port))
+  
+  for i in range(num_port):
+    wts = np.random.uniform(size = len(price_data.columns))
+    wts = wts/np.sum(wts)
+    all_wts[i,:] = wts
+    port_ret = np.sum(log_ret.mean() * wts)
+    port_ret = (port_ret + 1) ** 252 - 1
+    port_returns[i] = port_ret
+    port_sd = np.sqrt(np.dot(wts.T, np.dot(cov_mat, wts)))
+    port_risk[i] = port_sd
+    sr = port_ret / port_sd
+    sharpe_ratio[i] = sr
+    names = price_data.columns
+    min_var = all_wts[port_risk.argmin()]
+    max_sr = all_wts[sharpe_ratio.argmax()]
+    min_var = pd.Series(min_var, index=names)
+    max_sr = pd.Series(max_sr, index=names)
+    
+  st.title('Minimum Variance Portfolio')
+  st.bar_chart(min_var)
+  st.table(min_var)
+  
+  st.title('Maximum Sharpe Portfolio')
+  st.bar_chart(max_sr)
+  st.table(max_sr)
