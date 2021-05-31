d6a51407e8a8fbe5aee50ab06fe5df594ae0a028
mikecini <56175997+mikecini@users.noreply.github.com>
2021-05-31 12:05:52 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,7 @@ EDG = st.checkbox('Edgepoint Global Portfolio')
 DYN = st.checkbox('Dynamic Power Global Growth Portfolio')
 FID = st.checkbox('Fidelity Global Innovators')
 
-
+if GS:
+  st.checkbox('AGF Global Select', value = 'True')
+  GS_Returns = ffn.get('0P000073QD.TO')
+  st.line_chart(GS_Returns)
