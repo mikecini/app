890ec117bed6731da6341395d488ac1364b3af14
mikecini <56175997+mikecini@users.noreply.github.com>
2021-05-31 12:02:42 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import ffn
 
+GS = st.checkbox('AGF Global Select')
+EDG = st.checkbox('Edgepoint Global Portfolio')
+DYN = st.checkbox('Dynamic Power Global Growth Portfolio')
+FID = st.checkbox('Fidelity Global Innovators')
 
-stocks = ["0P000073QD.TO", "0P0000737Y.TO", "0P0001C8AE.TO", "0P0000IUYO.TO"]
-
-check_boxes = [st.sidebar.checkbox(stock, key=stock) for stock in stocks]
-checked_stocks = [stock for stock, checked in zip(stocks, check_boxes) if checked]
 
