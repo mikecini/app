e1c1d35552c07ae7d08a83fee597f99d6d8443d7
mikecini <56175997+mikecini@users.noreply.github.com>
2021-05-31 12:23:19 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,14 +7,14 @@ DYN = st.checkbox('Dynamic Power Global Growth Portfolio')
 FID = st.checkbox('Fidelity Global Innovators')
 
 if GS is True:
-  GS_Prices = ffn.get('0P000073QD.TO').to_returns()
+  GS_Prices = ffn.get('0P000073QD.TO').to_returns().na_drop()
   
 if EDG is True:
-  EDG_Prices = ffn.get('0P0000IUYO.TO')
+  EDG_Prices = ffn.get('0P0000IUYO.TO').to_returns().na_drop()
   
 if DYN is True: 
-  DYN_Prices = ffn.get('0P0000737Y.TO')
+  DYN_Prices = ffn.get('0P0000737Y.TO').to_returns().na_drop()
 
 if FID is True:
-  FID_Prices = ffn.get('0P0001C8AE.TO')
+  FID_Prices = ffn.get('0P0001C8AE.TO').to_returns().na_drop()
 
