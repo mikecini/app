9b9827c88ac1948e6d0a041d8b8c3513523f01fc
mikecini <56175997+mikecini@users.noreply.github.com>
2021-05-31 12:40:26 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,9 @@ max_weight = st.number_input('Maximum Weight')
 
 
 if GS and EDG is True:
-  GS_EDG = ffn.get('0P000073QD.TO,0P000070AC.TO').to_returns().dropna()
+  GS_EDG = ffn.get('0P000073QD.TO,0P0000IUYO.TO').to_returns().dropna()
   weights = ffn.core.calc_mean_var_weights(GS_EDG, weight_bounds=(min_weight, max_weight), rf=0.0)
+  st.table(GS_EDG)
   st.table (weights)
   
 if GS and DYN is True: 
